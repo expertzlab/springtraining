6ed78165753521030ec30163b06213c98153a6f0
Gireesh Babu <gireesh.babu@mozanta.com>
2017-01-10 07:14:27 +0530

    Tests added

--- src/main/java/com/expertzlab/readbook/BookController.java
+++ src/main/java/com/expertzlab/readbook/BookController.java
@@ -1,24 +1,38 @@
 package com.expertzlab.readbook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by gireeshbabu on 04/01/17.
  */
 
-@RestController
+@Controller
 @RequestMapping("/book")
 public class BookController {
+
+    private Logger logger = LoggerFactory.getLogger(BookController.class);
+    private MyProperties properties;
 
     @Autowired
     private BookRepo bookRepo;
 
+    @Autowired
+    public BookController(MyProperties properties){
+        this.properties = properties;
+    }
+
     @RequestMapping(value="/{isbn}", method = RequestMethod.GET)
-    public Book getBook(@PathVariable Isbn isbn){
-        return bookRepo.findBookByIsbn(isbn.getIsbn());
+    public String getBook(@PathVariable Isbn isbn, Model model){
+        logger.debug("Getting Id configured " + properties.getStudentId());
+        Book book = bookRepo.findBookByIsbn(isbn.getIsbn());
+        model.addAttribute("book",book);
+        return "bookview";
     }
 }
--- src/main/java/com/expertzlab/security/SecurityConfig.java
+++ src/main/java/com/expertzlab/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+@Profile("production")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -28,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
